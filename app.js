d13477d030d943ba895f54c1d692b33c7ce2c8f0
Okantey <nunoogideon19@gmail.com>
2022-12-23 15:47:34 +0000

    auto-type feature for roles

---
+++ app.js
@@ -0,0 +1,7 @@
+// auto type feature
+var typed = new Typed(".auto-type", {
+  strings: ["Frontend Developer.","UI/UX Product Designer.", "IT Consultant." ],
+  typeSpeed: 100,
+  backSpeed:10,
+  loop: true
+})
