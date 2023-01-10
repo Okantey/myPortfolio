866f3e5b4e33b1faff644d3d49a43d95771aac97
nii_okantey <nunoogideon19@gmail.com>
2023-01-10 20:59:35 +0000

    added pre-loader

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
+
+var loader = document.querySelector(".loader-cont");
+window.addEventListener("load", vanish);
+function vanish() {
+  loader.classList.add("disappear");
+}
+
+
+
+
 // auto type feature
 var typed = new Typed(".auto-type", {
-  strings: ["Frontend Developer.","UI/UX Designer.", "Freelancer." ],
+  strings: ["Frontend Developer.", "UI/UX Designer.", "Freelancer."],
   typeSpeed: 100,
-  backSpeed:10,
+  backSpeed: 10,
   loop: true
 })
 
@@ -11,13 +21,13 @@ var typed = new Typed(".auto-type", {
 const navicon = document.getElementById("navicon")
 const listItem = document.querySelector(".nav-list")
 
-navicon.addEventListener('click', function(){
+navicon.addEventListener('click', function () {
   listItem.classList.toggle("active")
   navbar.classList.toggle("active")
 
 
 
-  if(listItem.classList.contains("active")) {
+  if (listItem.classList.contains("active")) {
     navicon.className = "fa fa-close"
     navicon.style.fontSize = "2rem"
     navicon.style.cursor = "pointer"
@@ -33,10 +43,10 @@ const headerOptions = {
   rootMargin: "-300px 0px 0px 0px"
 }
 
-const headerObserver = new IntersectionObserver(function(entries, headerObserver) {
+const headerObserver = new IntersectionObserver(function (entries, headerObserver) {
   entries.forEach(entry => {
-    if(!entry.isIntersecting) {
-      navbar.classList.add("nav-scroll") 
+    if (!entry.isIntersecting) {
+      navbar.classList.add("nav-scroll")
     } else {
       navbar.classList.remove("nav-scroll")
     }
